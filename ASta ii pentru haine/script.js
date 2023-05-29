94ef150da7e1fb5961a0640a32fd0d901361e48c
luntras daniel <99122608+DANIELluntras@users.noreply.github.com>
2023-05-29 23:45:19 +0300

    Update script.js

--- ASta ii pentru haine/script.js
+++ ASta ii pentru haine/script.js
@@ -9,13 +9,23 @@
 
 
 
+ const cartIcon = document.querySelector("#cart-icon");
+ const cart = document.querySelector(".cart");
+ const closeCart = document.querySelector("#close-cart");
 
 
 
+//open Cart
+cartIcon.addEventListener("click", () => {
+    cart.classList.add("active"); 
+    console.log("clike")
+  });
 
+//Close Cart
 
-
-
+  closeCart.addEventListener("click", () => {
+    cart.classList.remove("active"); 
+  });
 
 
 
