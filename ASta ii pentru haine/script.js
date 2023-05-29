360d4a14e925a18d471d55e3540b4647e943b7f8
luntras daniel <99122608+DANIELluntras@users.noreply.github.com>
2023-05-29 23:33:48 +0300

    Create script.js

---
+++ ASta ii pentru haine/script.js
@@ -0,0 +1,31 @@
+// const cartIcon = document.querySelector("#cart-icon");
+// const cart = document.querySelector(".cart");
+
+
+
+// cartIcon.addEventListener("click", () => {
+//     cart.classList.add("show"); // Adaugă clasa CSS "show" pentru a afișa coșul de cumpărături
+//   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
